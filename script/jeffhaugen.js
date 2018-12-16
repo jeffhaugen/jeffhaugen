9e3aa4e2c614aa7b7987eb58d961947c59088989
Michal Ruopp <micruopp@gmail.com>
2018-12-15 20:39:25 -0600

    testing methods to embed posts

---
+++ script/jeffhaugen.js
@@ -0,0 +1,10 @@
+
+function main() {
+	console.log('hello, jeff')
+
+
+	let post = new File(["test"], "../posts/test.html")
+	console.log(post)
+}
+
+window.onload = main
---
+++ script/main.js
@@ -0,0 +1,16 @@
+
+// function main() {
+// 	console.log()
+// }
+
+
+
+// window.onload = main
+
+document.write('\
+\
+	<article class="box">\
+		<p>This was written by main.js</p>\
+	</article>\
+\
+')
